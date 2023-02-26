0df6c5d6e1c69cc7cc2a8cc1c0947d9513d2a930
Lorenzo Panieri <gokk99@gmail.com>
2023-02-26 01:34:07 +0100

    module header

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # stoej_dsp
-DSP extensions for JUCE
+
+This is an optional feature of the stoej extensions for juce. You can see the
+main documentation in the [stoej_core]() repository.
---
+++ stoej_dsp.h
@@ -0,0 +1,17 @@
+/*
+
+BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:                     stoej_dsp
+    vendor:                 stoej
+    version:                0.1.0
+    name:                   stoej_dsp
+    description:            DSP extensions for the JUCE framework
+
+    dependencies:           stoej_core, juce_audio_basics
+    license:                GPL3 / commercial
+    minimumCppStandard:     20
+
+END_JUCE_MODULE_DECLARATION
+
+*/
